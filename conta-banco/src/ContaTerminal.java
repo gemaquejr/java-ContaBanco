359033231976f6b728ae2feb239a2621d9db0f97
gemaquejr <gemaquejr@hotmail.com>
2024-04-29 19:45:30 -0300

    Adiciona desafio de projeto java conta bancária
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        //Solicitar e obter informações da conta do usuário
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer do scanner
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+
+        // Exibir mensagem de boas-vindas com as informações da conta
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
